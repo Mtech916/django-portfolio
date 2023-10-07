59cce2f79ba86b098c9e00020360e8cdf9904ca8
Mtech916 <shredtechfit@gmail.com>
2023-10-06 17:47:38 -0700

    Setup structure and page views, developing db

--- projects/urls.py
+++ projects/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from projects.views import HomePageView
+from projects.views import HomePageView, ProjectDetailView
 
 
 app_name = "projects"
 urlpatterns = [
+    path("project/<int:pk>", ProjectDetailView.as_view(), name="project-details"),
     path("", HomePageView.as_view(), name="homepage"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,26 @@
-from django.views.generic import TemplateView
+from django.views.generic import DetailView, TemplateView
+from projects.models import Project
+from django.urls import reverse
 
 
 class HomePageView(TemplateView):
-    template_name = "projects/index.html"
+    template_name = "projects/all_projects.html"
+
+    def get_context_data(self, **kwargs):
+        # Query the db to return all project objecst
+        context = super().get_context_data(**kwargs)
+        context["projects"] = Project.objects.all()
+        return context
+
+
+class ProjectDetailView(DetailView):
+    model = Project
+    template_name = "projects/project_detail.html"
+
+    def get_absolute_url(self):
+        return reverse("project-details")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["pk"] = self.object.pk
+        return context

86c6f793be38dd3d13eeba5393c86a05dfb29a3b
Mtech916 <shredtechfit@gmail.com>
2024-01-23 14:24:53 -0800

    trying to figure out psql config requirements

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -81,17 +81,23 @@ WSGI_APPLICATION = "portfolio.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "portfolio",
-        "USER": "postgres",
-        "PASSWORD": "webD3vChuker(3)",
-        "HOST": "localhost",
-        "Port": 5432,
+if os.environ.get("LOCAL_DEV", False):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "portfolio",
+            "USER": "postgres",
+            "PASSWORD": "webD3vChuker(3)",
+            "HOST": "localhost",
+            "Port": 5432,
+        }
     }
-}
+else:
+    import dj_database_url
 
+    DATABASES = {
+        "default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
